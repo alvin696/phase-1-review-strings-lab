ccce43e06cecf721ac06d42d16d4a817132a70fe
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-13 12:03:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Define the welcomeMessage variable using concatenation
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// Define the excitedWelcomeMessage variable using toUpperCase()
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+// Define the shortGreeting variable using string interpolation
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
